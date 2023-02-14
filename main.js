3885ef0fb3216f898527991e912d632697d3ef05
AntonioAnnes <123573311+AntonioAnnes@users.noreply.github.com>
2023-02-14 17:49:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
-const form = document.getElementById('form-valida');
-let valorA = parseFloat(document.getElementById('valor-compra').value)
-let valorB = parseFloat(document.getElementById('saldo-conta').value)
-
-formEvalido = false;
-
-function numerovalida (valorA, valorB){
-    return valorB > valorA
-}
-
-form.addEventListener('submit', function(e){
-
-    e.preventDefault();
-    const mensagemSucesso = `Conta feita com sucesso!`;
-
-    formEvalido = numerovalida(valor-compra.value, saldo-conta.value);
-    if (formEvalido) {
-        const containerMensagemSucesso = document.querySelector('sucess-message')
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block'
-
-        valorA.value = '';
-        valorB.value = '';
-
-    }
-    else {
-        document.querySelector('.error-message').style.display = 'block'
-    }
+$(document).ready(function(){
+    
+    $('.cadastra').on('click', function(){
+        const nameTarefa = $('#nomeTarefa').val()
+        const li = document.createElement('li')
+        li.innerHTML = `${nameTarefa}`
+        $('ul').append(li)
+        $('#nomeTarefa').val('')
+        $(li).click(function(){
+            $(li).toggleClass('feito')
+        })
+    })
 })
